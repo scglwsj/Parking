693592d537c968bd725187efe187260fedbaee33
scglwsj <scglwsj@163.com>
2019-03-31 23:12:38 +0800

    smart boy 停一辆车，他会找空位最多的那个停车场

--- Parking/Domain/Parks/Boys/SmartParkingBoy.cs
+++ Parking/Domain/Parks/Boys/SmartParkingBoy.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Parking.Domain.Cars;
+using Parking.Domain.Tickets;
 
 namespace Parking.Domain.Parks.Boys
 {
@@ -6,6 +9,12 @@ namespace Parking.Domain.Parks.Boys
     {
         public SmartParkingBoy(IList<Lot> parkingLots) : base(parkingLots)
         {
+        }
+
+        public new Ticket Park(Car car)
+        {
+            var parkingLot = _parkingLots.OrderByDescending(pl => pl.UsableParkingSpotNumber).First();
+            return parkingLot.Park(car);
         }
     }
 }
--- Test/Domain/Parks/SmartParkingBoyTest.cs
+++ Test/Domain/Parks/SmartParkingBoyTest.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace ParkingTest.Domain.Parks
 {
-public    class SmartParkingBoyTest
+    public class SmartParkingBoyTest
     {
         [Fact]
         public void Should_get_ticket_when_parking_boy_park_a_car()
@@ -18,6 +18,34 @@ public    class SmartParkingBoyTest
             var ticket = boy.Park(car);
 
             Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void Should_get_ticket_from_lot_1_when_smart_boy_park_and_park_1_is_more_empty_than_lot_2()
+        {
+            var lot1 = new Lot(2);
+            var lot2 = new Lot(1);
+            var boy = new SmartParkingBoy(new List<Lot> {lot1, lot2});
+            const string carId = "川A 123456";
+            var car = new Car(carId);
+
+            var ticket = boy.Park(car);
+
+            Assert.Equal(lot1.Id, ticket.LotId);
+        }
+
+        [Fact]
+        public void Should_get_ticket_from_lot_2_when_smart_boy_park_and_park_2_is_more_empty_than_lot_1()
+        {
+            var lot1 = new Lot(1);
+            var lot2 = new Lot(2);
+            var boy = new SmartParkingBoy(new List<Lot> { lot1, lot2 });
+            const string carId = "川A 123456";
+            var car = new Car(carId);
+
+            var ticket = boy.Park(car);
+
+            Assert.Equal(lot2.Id, ticket.LotId);
         }
     }
 }
