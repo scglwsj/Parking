a6b08129cd306f4157df87f360a1560061140c40
scglwsj <scglwsj@163.com>
2019-04-02 23:58:38 +0800

    ParkService 的 Park 是由它的 Parkable 执行的

---
+++ Parking/Application/Service/ParkApplicationService.cs
@@ -0,0 +1,20 @@
+﻿using Parking.Domain.ParkingBoys.Interface;
+using Parking.ValueObject;
+
+namespace Parking.Application.Service
+{
+   public class ParkApplicationService
+   {
+       private readonly IParkable _parkable;
+
+       public ParkApplicationService(IParkable parkable)
+       {
+           _parkable = parkable;
+       }
+
+       public void Park(Car car)
+       {
+           _parkable.Park(car);
+       }
+   }
+}
---
+++ Test/Application/Service/ParkApplicationServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Parking.Application.Service;
+using Parking.Domain.ParkingBoys.Interface;
+using Parking.ValueObject;
+using Xunit;
+
+namespace ParkingTest.Application.Service
+{
+    public class ParkApplicationServiceTest
+    {
+        [Fact]
+        public void Park_should_park_by_parkableMock()
+        {
+            var parkableMock = new Mock<IParkable>();
+            var service = new ParkApplicationService(parkableMock.Object);
+            var car = new Car("川A 123456");
+
+            service.Park(car);
+
+            parkableMock.Verify(pm => pm.Park(car), Times.Once);
+        }
+    }
+}
